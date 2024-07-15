2df7d00648deaac4e96c2515d8594e44bc7ab7a7
Nexterday <nexterday@users.noreply.github.com>
2024-07-15 09:39:14 +0200

    Add files via upload

--- lang/en/quizaccess_sebserver.php
+++ lang/en/quizaccess_sebserver.php
@@ -23,6 +23,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+ // phpcs:disable moodle.Files.LangFilesOrdering.IncorrectOrder
 
 defined('MOODLE_INTERNAL') || die();
 
